2362da924daca597ce6c4be5386c12c148071d7e
caixiaofei <876380459@qq.com>
2016-04-20 22:53:35 +0800

    test

---
+++ src/com/test/person.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class person {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s= "";
+		System.out.println(s.toString());
+
+	}
+
+}
