688463e8864c5512155df87fd2e5303e79c92c89
振辉 <@zhlee>
2016-04-21 23:02:24 +0800

    student

---
+++ src/com/test/Student.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Student {
+	private String id;
+	private String classno;
+	public final String getClassno() {
+		return classno;
+	}
+	public final void setClassno(String classno) {
+		this.classno = classno;
+	}
+
+}
