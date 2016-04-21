ffb2133cb8a5e566a6ce72c2e9c28e67d5da4d44
振辉 <@zhlee>
2016-04-21 23:05:35 +0800

    dept

---
+++ src/com/test/Dept.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Dept {
+	private String deptno;
+	private String deptname;
+	public String getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(String deptno) {
+		this.deptno = deptno;
+	}
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+
+}
