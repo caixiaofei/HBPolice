431387f44a903bc0191438a6064d5ef81a93b773
caidafei <15038046547@126.com>
2016-04-21 22:08:40 +0800

    ceshilei

--- src/com/test/person.java
+++ src/com/test/person.java
@@ -1,6 +1,29 @@
 package com.test;
 
 public class person {
+	private String name;
+	private int age;
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 
 	/**
 	 * @param args
@@ -8,7 +31,19 @@ public class person {
 	public static void main(String[] args) {
 		String s= "";
 		System.out.println(s.toString());
+		
+		person p = new person();
+		p.setAge(25);
+		p.setName("caifei");
+		System.out.println(p.toString());
+		
 
+	}
+
+
+	@Override
+	public String toString() {
+		return "person [name=" + name + ", age=" + age + "]";
 	}
 
 }
